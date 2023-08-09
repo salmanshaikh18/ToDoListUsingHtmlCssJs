230dfe81304ec393dbdc9243bf1b5f1121484094
salmanshaikh18 <salmanshaikh18786@gmail.com>
2023-08-09 23:50:12 +0530

    display todo, delete todo

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const addUserBtn = document.getElementById('addUserBtn')
 
 const userNameTextField = document.getElementById('userName')
 
+let recordDisplay = document.getElementById('records')
+
 let usersArray = []
 
 let objStr = localStorage.getItem('users')
@@ -9,6 +11,8 @@ let objStr = localStorage.getItem('users')
 if (objStr != null) {
     usersArray = JSON.parse(objStr)
 }
+
+displayInfo();
 
 addUserBtn.addEventListener('click', () => {
     const userName = userNameTextField.value;
@@ -26,19 +30,30 @@ function saveInfo(usersArray) {
     let str = JSON.stringify(usersArray)
     // save in localStorage
     localStorage.setItem('users', str)
+    displayInfo()
 }
 
 function displayInfo() {
     let statement = '';
-    usersArray.forEach((user) => {
-        
+    usersArray.forEach((user, i) => {
+        statement += `<tr>
+        <th scope="row">${i+1}</th>
+        <td>${user.name}</td>
+        <td>
+            <i class="fa fa-edit" onclick='editInfo(${i})'style="font-size:30px"></i>
+            <i class="fa fa-trash-o" onclick='deleteInfo(${i})' style="font-size:30px; color:red"></i>
+        </td>
+    </tr>`
     })
+    recordDisplay.innerHTML = statement
 }
 
-function editInfo() {
-
+function editInfo(id) {
+    
 }
 
-function deleteInfo() {
-
+function deleteInfo(id) {
+    usersArray.splice(id,1)
+    saveInfo(usersArray)
+    displayInfo()
 }
